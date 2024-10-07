619613c6eda78f3ce112da08a66fb49452f9420e
Valentina070 <valentina.zuniga.vazquez@gmail.com>
2024-10-07 15:36:36 -0500

    Creación y propiedades de main

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.sql.Date;
+
 public class Main {
     public static void main(String[] args) {
        
@@ -24,7 +26,17 @@ public class Main {
        
        
        
-       
+    Capitan cabina = new Capitan("Neyder Ruiz", "1128282344", 10);
+    capitan.mostrarDatos();
+    System.out.println("");
+    Reserva reserva= new Reserva("Rose Dominguez", 2000000, "Gold VIP",  Date.valueOf("1998-10-07"),  1005);
+    reserva.mostrarDatos();
+    System.out.println("");
+    Tripulacion tripulacion = new Tripulacion("Mayor", "Luis Santodomingo","112324156");
+    tripulacion.mostrarDatos();
+    System.out.println("");
+    Restaurante restaurante = new Restaurante("Cactus", 30, "Ensaladas", "Comida saludable");
+    restaurante.mostrarDatos();
        
        
        
@@ -58,5 +70,9 @@ public class Main {
 
 
         
+    
+    
+    
+    
     }
 }
